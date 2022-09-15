12563de0414b97ab501c9a5c86c78253e8bce6b3
Amjad Oudeh <74506206+Amjadoudeh@users.noreply.github.com>
2022-09-15 20:09:06 +0200

    First commit

---
+++ Cryptofiy/CryptofiyApp.swift
@@ -0,0 +1,17 @@
+//
+//  CryptofiyApp.swift
+//  Cryptofiy
+//
+//  Created by Amjad Oudeh on 15.09.22.
+//
+
+import SwiftUI
+
+@main
+struct CryptofiyApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
