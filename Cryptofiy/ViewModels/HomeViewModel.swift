80f613676760de85517ca8f7bee45ef3798e7713
Amjad Oudeh <74506206+Amjadoudeh@users.noreply.github.com>
2022-09-18 21:26:01 +0200

    Subscrib to the Network from the viewModel and show the data

--- Cryptofiy/ViewModels/HomeViewModel.swift
+++ Cryptofiy/ViewModels/HomeViewModel.swift
@@ -1,13 +1,27 @@
 import Foundation
+import Combine
 
 class HomeViewModel: ObservableObject {
     @Published var allCoins: [CoinModel] = []
     @Published var protfiloCoins: [CoinModel] = []
     
+    private let dataService = CoinDataServices()
+    private var cancellables = Set<AnyCancellable>()
+    
     init() {
-        DispatchQueue.main.asyncAfter(deadline: .now() + 2.0) {
-            self.allCoins.append(DeveloperPreview.instance.coin)
-            self.protfiloCoins.append(DeveloperPreview.instance.coin)
-        }
+        addSubscribers()
+        // to test the UI
+//        DispatchQueue.main.asyncAfter(deadline: .now() + 2.0) {
+//            self.allCoins.append(DeveloperPreview.instance.coin)
+//            self.protfiloCoins.append(DeveloperPreview.instance.coin)
+//        }
+    }
+    
+    func addSubscribers() {
+        dataService.$allCoins
+            .sink { [weak self] (returnedCoins) in
+                self?.allCoins = returnedCoins
+            }
+            .store(in: &cancellables)
     }
 }
