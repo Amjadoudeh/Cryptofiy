6d9b4f0a89761ecc6d877d067c3bf4d0aa48380b
Amjad Oudeh <74506206+Amjadoudeh@users.noreply.github.com>
2022-09-19 11:02:18 +0200

    divide the network request to blocks of functions to make it clean reusable

--- Cryptofiy/Services/NetworkManager.swift
+++ Cryptofiy/Services/NetworkManager.swift
@@ -15,4 +15,13 @@ class NetworkingManager {
             .receive(on: DispatchQueue.main)
             .eraseToAnyPublisher()
     }
+    
+    static func handleCompletion(completion: Subscribers.Completion<Error>) {
+        switch completion {
+        case . finished:
+            break
+        case .failure(let error):
+            print(error.localizedDescription)
+        }
+    }
 }
