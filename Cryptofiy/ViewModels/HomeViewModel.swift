54e6bd11dc416c502d3c09fa1fb63fc6c2208dfc
Amjad Oudeh <74506206+Amjadoudeh@users.noreply.github.com>
2022-09-19 16:49:35 +0200

    Add a search method

--- Cryptofiy/ViewModels/HomeViewModel.swift
+++ Cryptofiy/ViewModels/HomeViewModel.swift
@@ -11,10 +11,10 @@ class HomeViewModel: ObservableObject {
     init() {
         addSubscribers()
         // to test the UI
-//        DispatchQueue.main.asyncAfter(deadline: .now() + 2.0) {
-//            self.allCoins.append(DeveloperPreview.instance.coin)
-//            self.protfiloCoins.append(DeveloperPreview.instance.coin)
-//        }
+        //        DispatchQueue.main.asyncAfter(deadline: .now() + 2.0) {
+        //            self.allCoins.append(DeveloperPreview.instance.coin)
+        //            self.protfiloCoins.append(DeveloperPreview.instance.coin)
+        //        }
     }
     
     func addSubscribers() {
@@ -23,5 +23,23 @@ class HomeViewModel: ObservableObject {
                 self?.allCoins = returnedCoins
             }
             .store(in: &cancellables)
+        $searchText
+            .combineLatest(dataService.$allCoins)
+            .map { (text, startingCoins) ->[CoinModel] in
+                guard !text.isEmpty else { return startingCoins }
+                
+                let lowercasedText = text.lowercased()
+                
+                return startingCoins.filter {(coin) -> Bool in
+                    return coin.name.lowercased().contains(lowercasedText) ||
+                    coin.symbol.lowercased().contains(lowercasedText) ||
+                    coin.id.lowercased().contains(lowercasedText)
+                }
+            }
+            .sink { [weak self] (returnedCoins) in
+                self?.allCoins = returnedCoins
+            }
+            .store(in: &cancellables)
     }
 }
+
